47db00044b6b72823f31a887215dd34100cffa00
jeryTymate <jeryTymate@users.noreply.github.com>
2017-11-02 11:43:35 +0100

    Fix syntax postfix text

--- GDPerformanceView-Swift/GDPerformanceMonitoring/GDPerformanceMonitor.swift
+++ GDPerformanceView-Swift/GDPerformanceMonitoring/GDPerformanceMonitor.swift
@@ -57,7 +57,9 @@ public class GDPerformanceMonitor: NSObject {
 	Change it to add a custom text at the end of debug info.
 	*/
 	public var postfixText: String = "" {
-		self.performanceView.postfixText = self.postfixText
+	    didSet {
+		self.performanceView?.postfixText = self.postfixText
+       	    }
 	}
     
     /**
